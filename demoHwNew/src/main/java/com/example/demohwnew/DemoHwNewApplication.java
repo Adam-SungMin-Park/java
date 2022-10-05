dc3d43d8f4acc682d91358845eb0872ec77d5236
Adam Sung Min Park <parksm2@uci.edu>
2022-10-05 18:06:51 +0900

    Update DemoHwNewApplication.java

--- demoHwNew/src/main/java/com/example/demohwnew/DemoHwNewApplication.java
+++ demoHwNew/src/main/java/com/example/demohwnew/DemoHwNewApplication.java
@@ -15,25 +15,7 @@ import java.util.List;
 @SpringBootApplication
 @EnableJpaAuditing
 public class DemoHwNewApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DemoHwNewApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner seedData(PostRepository postRepository, PostService postService){
-        return (args -> {
-            postRepository.save(new Post("adam","testing content","adam@gmail.com","1234"));
-            System.out.println("data coming thru");
-            List<Post> postList = postRepository.findAll();
-            for(int i = 0 ; i < postList.size(); i++){
-                Post post = postList.get(i);
-                System.out.println(post.getId());
-                System.out.println(post.getName());
-                System.out.println(post.getContent());
-            }
-            postRepository.save(new Post("2adam2","2testing content2","adam@gmail.com","1234"));
-
-        });
     }
 }
